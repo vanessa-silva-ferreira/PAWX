6a2fa35e2178bb97a9c894fa1313750acca0cea0
vanessa-silva-ferreira <Vanessa.Ferreira.T0127547@edu.atec.pt>
2024-10-12 19:28:38 +0100

    login

--- PAWX/routes/web.php
+++ PAWX/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'authenticate']);
