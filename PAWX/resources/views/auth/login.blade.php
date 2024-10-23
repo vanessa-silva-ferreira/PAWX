f2b3d2de6567cd924ecacb3bb814daaa2c20407e
daniel-santos11 <daniel.santos.t0127534@edu.atec.pt>
2024-10-23 22:20:09 +0100

    Login fronted sketch v2

--- PAWX/resources/views/auth/login.blade.php
+++ PAWX/resources/views/auth/login.blade.php
@@ -10,23 +10,30 @@
             <div class="flex items-center justify-center min-h-screen bg-gray-100">
                 <div class="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl
                 md:flex-row md:space-y-0">
-                    <div class="relative">
+                    <div class="relative object-cover sm:hidden md:block">
                         <img src="{{asset('images/image.jpg')}}" alt="img" class="
-                        w-[400px] h-full hidden rounded-l-2xl md:block object-cover"/>
-                        <div class="absolute hidden bottom-10 right-6 p-6">
-                            <span class="text-white text-4xl">PAWX</span>
+                        w-[400px] h-full rounded-l-2xl"/>
+                        <div class="absolute bottom-10 right-6 p-6 rounded drop-shadow-lg md:block">
+                            <span class="text-white text-8xl">PAWX</span>
                         </div>
                     </div>
                     <div class="flex flex-col justify-center p-8 md:p-14">
-                        <span class="mb-3 text-4xl font-bold">Welcome to PAWX</span>
-                        <span class="font-light text-gray-400 mb-8">Please enter your details</span>
+                        <div class="flex w-full">
+                            <button class="bg-orange-500 text-white font-bold py-2 px-4 rounded-l-md">
+                                Sign Up
+                            </button>
+                            <button class="bg-gray-200 text-gray-400 font-bold py-2 px-4 rounded-r-md">
+                                Sign In
+                            </button>
+                        </div>
+
                         <div class="py-4">
-                            <span class="mb-2 text-md">Email</span>
+                            <span class="mb-2 text-md text-orange-500">Email</span>
                             <input type="text" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light
                             placeholder:text-gray-500 bg-white" name="email" id="email"/>
                         </div>
                         <div class="py-4">
-                            <span class="mb-2 text-md">Password</span>
+                            <span class="mb-2 text-md text-orange-500">Password</span>
                             <input type="password" name="pass" id="pass2" class="w-full p-2 border border-gray-300 rounded-md
                             placeholder:font-light placeholder:text-gray-500 bg-white"/>
                         </div>
@@ -37,18 +44,14 @@
                             </div>
                             <span class="font-bold text-md text-black">Esqueci-me da password</span>
                         </div>
-                        <button class="w-full bg-orange-600 text-white p-2 rounded-lg
-                        mb-6 hover:bg-white hover:text-orange-600 hover:border hover:border-orange-600">
+                        <button class="w-full bg-orange-500 text-white p-2 rounded-lg
+                        mb-6 hover:bg-white hover:text-orange-500 hover:border hover:border-orange-500">
                             Log in
                         </button>
-                        <button class="w-full border border-orange-600 text-md text-black p-2 rounded-lg mb-6
+                        <button class="w-full border border-orange-500 text-md text-black p-2 rounded-lg mb-6
                         hover:bg-black hover:text-white">
                             Sign in with Google
                         </button>
-                        <div class="text-center text-orange-600">
-                            Não tem conta?
-                            <span class="font-bold text-orange-600">Registe-se gratuitamente</span>
-                        </div>
                     </div>
                 </div>
             </div>
