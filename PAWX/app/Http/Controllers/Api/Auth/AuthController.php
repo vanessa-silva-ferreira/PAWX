ddb3cdade0a4d93d85685beaf6a74e50f4513306
Gonçalo Barros <Goncalo.Barros.T0127538@edu.atec.pt>
2024-10-31 23:48:55 +0000

    Login via API (failed)

--- PAWX/app/Http/Controllers/Api/Auth/AuthController.php
+++ PAWX/app/Http/Controllers/Api/Auth/AuthController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            if($user)
+            {
+                try {
+                    $success['token'] =  $user->createToken('userToken')-> plainTextToken;
+                    return response($success, 'User login successfully.');
+                } catch (\Exception $e) {
+                    return response()->json(['error' => 'Could not create token: ' . $e->getMessage()], 500);
+                }
+            }
+            else {
+                return response()->json(['error' => 'Not a user!'], 401);
+            }
+        }
+        else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
 }
--- PAWX/routes/api.php
+++ PAWX/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\AuthController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+
+
+Route::post('/login', [AuthController::class, 'login']);
