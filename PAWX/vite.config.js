672afbb29285bbcf33999036889991cb5325d2c5
vanessa-silva-ferreira <Vanessa.Ferreira.T0127547@edu.atec.pt>
2024-12-01 06:37:47 +0000

    public/build/ assets and manifest.json + removing css and js from public (only slightly working) + vite config: problem with the other branch where this was done

--- PAWX/vite.config.js
+++ PAWX/vite.config.js
@@ -4,8 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+    },
+    server: {
+        watch: {
+            ignored: ['**/vendor/**', '**/node_modules/**']
+        },
+    },
 });
