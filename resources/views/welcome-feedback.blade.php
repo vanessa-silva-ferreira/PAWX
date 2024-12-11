8d58017e8ada83e1fc4d23d08838cf43699ce0d8
Vanessa <157629723+vanessa-silva-ferreira@users.noreply.github.com>
2024-12-11 17:00:22 +0000

    fix tw

--- resources/views/welcome-feedback.blade.php
+++ resources/views/welcome-feedback.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PAWX - Feedback dos Clientes</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-900 font-sans text-gray-100">
