ce6b993bf7480d3dc746b458a8e9143230ec723a
Gonçalo Barros <Goncalo.Barros.T0127538@edu.atec.pt>
2024-12-11 22:40:47 +0000

    Edit appointments (admin)

--- app/Traits/AppointmentValidationRules.php
+++ app/Traits/AppointmentValidationRules.php
@@ -2,13 +2,17 @@
 
 namespace App\Traits;
 
-
-
 trait AppointmentValidationRules
 {
-    public function appointmentRules()
+    /**
+     * Regras de validação para a criação ou atualização de uma marcação.
+     *
+     * @param bool $isUpdate Indica se é uma atualização.
+     * @return array
+     */
+    public function appointmentRules(bool $isUpdate = false)
     {
-        return[
+        $rules = [
             'pet_id' => 'required|exists:pets,id',
             'employee_id' => 'required|exists:employees,id',
             'service_id' => 'required|exists:services,id',
@@ -16,8 +20,22 @@ trait AppointmentValidationRules
             'status' => 'required|string|max:255',
             'total_price' => 'required|numeric|min:0',
         ];
+
+        if ($isUpdate) {
+            $rules['pet_id'] = 'nullable|exists:pets,id';
+            $rules['employee_id'] = 'nullable|exists:employees,id';
+            $rules['service_id'] = 'nullable|exists:services,id';
+        }
+
+        return $rules;
     }
 
+    /**
+     * Extrai os dados relevantes para uma marcação de um array de dados.
+     *
+     * @param array $requestData
+     * @return array
+     */
     public function extractAppointmentData(array $requestData): array
     {
         return array_intersect_key($requestData, array_flip([
